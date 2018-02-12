6f1f1a507c21f8032f32ca9cb5119ff68d3eb97e
Cristian <cristian>
2018-02-12 16:48:09 -0500

    navigate login

--- src/app/services/guards/login-guard.guard.ts
+++ src/app/services/guards/login-guard.guard.ts
@@ -1,16 +1,20 @@
 import { Injectable } from '@angular/core';
-import { CanActivate } from '@angular/router';
+import { CanActivate, Router } from '@angular/router';
 import { UserService } from '../uses/user.service';
 
 @Injectable()
 export class LoginGuardGuard implements CanActivate {
-  constructor( public _userSer: UserService) {  }
+  constructor(
+    public _userSer: UserService,
+    public _router: Router
+  ) {  }
 
   canActivate() {
     if ( this._userSer.isLogin() ) {
       console.log('Paso por el guard');
       return true;
     } else {
+      this._router.navigate(['/login']);
       console.log('Bloqueado por el guard');
       return false;
     }
